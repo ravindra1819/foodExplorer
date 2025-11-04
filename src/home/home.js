1cc635b589a58ff0af9eaf612935427817fe3f38
Ravindra <ravindra.reddy1812@gmail>
2025-11-04 18:42:25 +0530

    setup&fetch of API | Browser category

---
+++ src/home/home.js
@@ -0,0 +1,45 @@
+const APIData = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+
+document.addEventListener('DOMContentLoaded', () => {
+  foodRawData();
+});
+
+async function foodRawData() {
+
+  // const homeIcon = document.getElementById('homeIcon');
+  // const homeField = document.getElementById('homeField');
+  // const searchField = document.getElementById('searchField');
+
+  try{
+    const response = await fetch(APIData);
+    const data = await response.json();
+    renderFoodItems(data.categories);
+  }
+  catch(error){
+    console.error('Error Fetching data:',error);
+  }
+
+}
+
+function renderFoodItems (categories = []){
+const categoryContainer = document.getElementById('categoryContainer');
+categoryContainer.innerHTML = "";
+
+ if (categories.length === 0){
+  categoryContainer.innerHTML = '<p>No Categories are present to display</p>'
+  return;
+ }
+
+ categories.forEach((category) => {
+  const { strCategory, strCategoryThumb, strCategoryDescription } = category;
+  const card = document.createElement('div')
+  card.classList.add("itemCard");
+  card.innerHTML = `
+  <img src="${strCategoryThumb}" alt="${strCategory}" />
+  <h3>${strCategory}</h3>
+  <p>${strCategoryDescription}</p>
+  `;
+
+categoryContainer.appendChild(card);
+ })
+}
